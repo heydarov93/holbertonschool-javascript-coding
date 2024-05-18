5e094678b6d63bae9269393f64e5f11907da78f4
heydarov93 <heyderov.yashar@mail.ru>
2024-05-18 21:21:58 +0400

    error handling

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,7 @@ import process from 'process';
 
 const URL = process.argv[2];
 
-request(URL, function (err, response, body) {
-  console.log(`code: ${response.statusCode}`);
+request(URL, (err, response, body) => {
+  if (!err)
+    console.log(`code: ${response.statusCode}`);
 });
