077a8cb1102848f96839958ac75b1324c78e974a
heydarov93 <heyderov.yashar@mail.ru>
2024-06-02 13:38:21 +0400

    lint errors fixed

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,7 @@
-
 const readline = require('node:readline');
-const {stdin: input, stdout: output} = require('node:process');
+const { stdin: input, stdout: output } = require('node:process');
 
-const rl = readline.createInterface({input, output});
+const rl = readline.createInterface({ input, output });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
   console.log(`Your name is: ${answer}\n`);
