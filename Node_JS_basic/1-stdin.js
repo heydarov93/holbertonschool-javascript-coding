d1eb1769f4200f7cf96625fec4e64ee57bef3b45
heydarov93 <heyderov.yashar@mail.ru>
2024-06-02 13:59:04 +0400

    readline.write used instead of console.log

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,8 +4,8 @@ const { stdin: input, stdout: output } = require('node:process');
 const rl = readline.createInterface({ input, output });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  console.log(`Your name is: ${answer}\n`);
+  rl.write(`Your name is: ${answer}\r`);
 
+  rl.write('This important software is now closing\n');
   rl.close();
-  console.log('This important software is now closing');
 });
