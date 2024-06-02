e2d362b03e2e616d5101ff3fa2605ecd9d633720
heydarov93 <heyderov.yashar@mail.ru>
2024-06-02 13:37:23 +0400

    newline added at the end of output

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ const {stdin: input, stdout: output} = require('node:process');
 const rl = readline.createInterface({input, output});
 
 rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  console.log(`Your name is: ${answer}`);
+  console.log(`Your name is: ${answer}\n`);
 
   rl.close();
   console.log('This important software is now closing');
