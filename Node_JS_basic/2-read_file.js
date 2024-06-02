01a434d53773c38ab24a211c8cca919168f7e6ec
heydarov93 <heyderov.yashar@mail.ru>
2024-06-02 16:47:43 +0400

    Reading a file synchronously with Node JS

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const fs = require('node:fs');
+
+function countStudents(path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+
+    const parsedData = data.trim().split('\n').slice(1).map((str) => str.split(','));
+    const numberOfStudents = parsedData.length;
+    const fieldNames = Array.from(new Set(parsedData.map((row) => row.at(-1))));
+    const studentsByFields = {};
+
+    fieldNames.forEach((field) => {
+      studentsByFields[field] = [];
+    });
+
+    parsedData.forEach((studentInfo) => {
+      studentsByFields[studentInfo.at(-1)].push(studentInfo[0]);
+    });
+
+    console.log(`Number of students: ${numberOfStudents}`);
+
+    Object.keys(studentsByFields).forEach((data) => {
+      const output = `Number of students in ${data}: ${studentsByFields[data].length} `
+      + `List: ${studentsByFields[data].join(', ')}`;
+
+      console.log(output);
+    });
+  });
+}
+
+module.exports = countStudents;
